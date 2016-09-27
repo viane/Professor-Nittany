4d54c4ca2caf0941313261f9df15a7b920874dd0
xiaoyu zhou <xiaoyuz2011@gmail.com>
2016-09-27 08:16:22 -0400

    Add evernote route

--- app/routes.js
+++ app/routes.js
@@ -187,7 +187,7 @@ module.exports = function(app, passport) {
 			successRedirect: '/profile',
 			failureRedirect: '/'
 		}));
-	
+		
 	// =====================================
 	// EVERNOTE ROUTES =====================
 	// =====================================
