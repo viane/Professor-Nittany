3e8bd6a9e5b07848aee13030f015062261a87e64
xiaoyu zhou <xiaoyuz2011@gmail.com>
2017-01-14 21:03:54 -0500

    add log to deserialize user id

--- config/passport.js
+++ config/passport.js
@@ -29,11 +29,12 @@ module.exports = function(passport) {
 
     // used to serialize the user for the session
     passport.serializeUser(function(user, done) {
-        done(null, user._id);
+        done(null, user.id);
     });
 
     // used to deserialize the user
     passport.deserializeUser(function(id, done) {
+      console.log("Try to deserialize user, user id is ", id);
         User.findById(id, function(err, user) {
             done(err, user);
         });
