2ecef1544a75249d1ba2bf628884d78ac1709c13
xiaoyu zhou <xiaoyuz2011@gmail.com>
2017-01-16 23:19:21 -0500

    fix typo

--- config/console.js
+++ config/console.js
@@ -3,12 +3,13 @@ const fs = require('fs');
 const moment = require('moment');
 const now = moment();
 
-//override console.log function to log to /debug.log and console
-var log_file = fs.createWriteStream('app/log/console_log.txt', {flags: 'w'});
-var log_stdout = process.stdout;
-console.log = function(d) { //
+//override console.log function
 
-    //in production, log to DB
+const log_stdout = process.stdout;
+
+console.log = function(d) {
+
+    //in production, also log to DB
 
     log_stdout.write(now.format("YYYY-MM-DD HH:mm:ss") + " " + util.format(d) + '\n');
 };
