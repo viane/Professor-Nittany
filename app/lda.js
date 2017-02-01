8d6a256285495d6fb86c58f6184066ac66876769
xiaoyu zhou <xiaoyuz2011@gmail.com>
2017-02-01 00:18:49 -0500

    fix typo

--- app/lda.js
+++ app/lda.js
@@ -2,7 +2,9 @@
 
 const lda = require('lda');
 
-module.exports.getTopic = function (input, categories = 1, terms= 10) {
+module.exports.getTopic = function (input, categories, terms) {
+    if(!categories) categories = 2;
+    if(!terms) terms = 5;
     const documents = input.match( /[^\.!\?]+[\.!\?]+/g );
     const result = lda(documents, categories, terms);
     return result;
