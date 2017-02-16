a40a4ea352cd393106dc26638e40dcca571abd4e
xiaoyu zhou <xiaoyuz2011@gmail.com>
2017-02-16 18:47:49 -0500

    fix set interval argument bug

--- app/server-status.js
+++ app/server-status.js
@@ -13,7 +13,6 @@ module.exports.updateStatsFromQuestionObj = function(questionObj) {
 
 // get recent questionFeeds
 module.exports.getRecentAskedQuestions = () => {
-    console.log("Client requesting question feeds from server, current question feeds is ", questionFeeds);
     return questionFeeds;
 };
 
@@ -23,12 +22,12 @@ module.exports.initQuestionFeeds = () => {
     return new Promise((resolve, reject) => {
         loadJsonFile(appRoot + serverStatusPath).then(json => {
             questionFeeds = json.recent_asked_question;
-
             // write the question feeds to disk every 1 hour
-            setInterval(writeQuestionFeeds().catch((err) => {
-                throw err
-            }), 3600000);
-
+            setInterval(() => {
+                writeQuestionFeeds().catch((err) => {
+                    throw err
+                })
+            }, 3600000);
             resolve();
         }).catch((err) => {
             throw err;
@@ -39,7 +38,6 @@ module.exports.initQuestionFeeds = () => {
 
 // update questionFeeds
 module.exports.updateRecentAskedQuestions = (Question) => {
-    console.log("adding ", Question, " to server question feeds");
     // 3 cases, the question is new, already in the list but still asked recently, already asked but long time ago
     if (!questionFeeds.includes(Question)) {
         // server only store max 50 of recently asked questions
@@ -58,24 +56,18 @@ module.exports.updateRecentAskedQuestions = (Question) => {
 // return a promise
 const writeQuestionFeeds = () => {
     return new Promise((resolve, reject) => {
-
         // read last copy from disk
-
         loadJsonFile(appRoot + serverStatusPath).then(json => {
-
             json.recent_asked_question = questionFeeds;
-
             writeJsonFile(appRoot + serverStatusPath, json).then(() => {
                 resolve();
             }).catch((err) => {
                 throw err;
                 reject(err);
             })
-
         }).catch((err) => {
             throw err;
             reject(err);
         })
-
     })
 };
