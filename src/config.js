48b4e61078e8cbf617d352607207592c8ee22a93
kzw5309 <harryhappy111@gmail.com>
2017-07-18 12:59:19 -0400

    update user login

--- src/config.js
+++ src/config.js
@@ -6,6 +6,50 @@ module.exports = {
     clientSecret: '26f046e8112ae5048435cf7ef1e6c1ac',
     callbackURL: 'https://localhost:3443/users/facebook/callback'
   },
+  'twitter' : {
+    'consumerKey'     : 'NEKSGvDlA6Hz63BFaPYfshOEA',
+    'consumerSecret'  : 'lxw7wAvKo90IW0zZXi9qKqlKXyyWRxV8gEPgE5ylknIlbV2Y0n',
+    'callbackURL'     : 'https://localhost:3443/users/twitter/callback'
+  },
+
+  'google' : {
+    'clientID'    : '132722948922-gavv9i3p51t0heg27qihaj1c4kocn01h.apps.googleusercontent.com',
+    'clientSecret'  : 'ZTKGjktIOUz3-m1L0BCBud_9',
+    'callbackURL'   : 'https://localhost:3443/users/google/callback'
+  },
+
+  'linkedin' : {
+    'clientID'    : '78y1qxhj1ass0f',
+    'clientSecret'  : 'gbyTOaNTCXegTufn',
+    'callbackURL'   : 'https://localhost:3443/users/linkedin/callback'
+  },
+
+  'instagram' : {
+    'clientID'    : 'c36ed58b9ea848deb94520f896d5a510',
+    'clientSecret'  : 'bb4cf3b16e3a4188aa3a875324e66f74',
+    'callbackURL'   : 'https://intelligent-student-advisor.herokuapp.com/auth/instagram/callback'
+  },
+
+  'reddit' : {
+    'clientID'    : 'yo-BqUiqPXuSXA',
+    'clientSecret'  : 'vyY0HbuiunObb4eGT-lEkx4CwyM',
+    'callbackURL'   : 'https://intelligent-student-advisor.herokuapp.com/auth/reddit/callback',
+    'avatar'    : '/img/reddit-icon.png'
+  },
+
+  'amazon' : {
+    'clientID'    : 'amzn1.application-oa2-client.d1e9e34ce1d24f3a9b68e05f56e9ea73',
+    'clientSecret'  : '75f16bbe34c4f5529a81a0e8df431e8f163ed28ee9b5b2e6083961ad1446cf03',
+    'callbackURL'   : 'https://intelligent-student-advisor.herokuapp.com/auth/amazon/callback',
+    'avatar'    : '/img/amazon-icon.png'
+  },
+
+  'wechatAu': {
+    'clientID': 'viane2222',
+    'clientSecret': 'fada9ec6fa020548',
+    'callbackURL': 'https://intelligent-student-advisor.herokuapp.com/auth/wechat/callback',
+    'avatar': '/img/wechat-icon.png'
+  },
   'watson': {
     retrieve_and_rank: {
       username: 'a658754c-09f3-4cc4-a8a1-198a25cd295e',
